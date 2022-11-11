ee33ec456a3919a58102d5aaced047aa941a387c
dumbu1 <krishnamohan.balivada@gmail.com>
2022-11-11 23:21:51 +0530

    first commit

---
+++ CompleteProjectStructure/src/test/java/stepDefinations/DataDriven_SD.java
@@ -0,0 +1,47 @@
+package stepDefinations;
+
+
+import org.openqa.selenium.WebDriver;
+
+import commonMethods.SeleniumSpecificMethods;
+import io.cucumber.java.en.*;
+import pages_objects.HomePage;
+import pages_objects.SeleniumTestExamplesPage;
+import utility.DriverManager;
+
+
+public class DataDriven_SD {
+	
+	
+	
+	@Given("user is on {string} page")
+	public void user_is_on_home_page_with_title(String expectedPageTitle) {
+		SeleniumSpecificMethods.validate_PageTitle(expectedPageTitle);
+	}
+	
+	
+	@When("user clicks on {string} page")
+	public void user_clicks_on_page(String string) {
+	    
+	  HomePage obj_HomePage=new HomePage(DriverManager.getDriver());
+	    obj_HomePage.click_link();
+	}
+	@When("user enters names on text box and clicks on Submit button")
+	public void user_enters_on_text_box_and_clicks_on_button() {
+
+	SeleniumTestExamplesPage obj_SeleniumTestExamplesPage=new SeleniumTestExamplesPage(DriverManager.getDriver());
+	obj_SeleniumTestExamplesPage.fillTheForm();
+	}
+	
+	@When("user enters names on text box as per {int} and clicks on Submit button")
+	public void user_enters_names_on_text_box_as_per_and_clicks_on_submit_button(Integer rowNumber) {
+		
+		System.out.println("user is on*************user enters names on text box as per {int} and clicks on Submit button ");
+		SeleniumTestExamplesPage obj_SeleniumTestExamplesPage=new SeleniumTestExamplesPage(DriverManager.getDriver());
+		obj_SeleniumTestExamplesPage.filltheForm(rowNumber);
+		
+		
+	}
+	
+
+}
---
+++ CompleteProjectStructure/src/test/java/testRuns/TestRunnerForDataDriven.java
@@ -0,0 +1,42 @@
+package testRuns;
+
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+/*@ExtendedCucumberOptions(
+		
+		retryCount = 3
+		)*/
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+
+		features="src/test/resources/features/dataDrivenTesting/feature1.feature",
+
+
+		glue = {"stepDefinations","hooks"},
+
+		/*plugin = {"pretty",
+
+
+				//for exten reports
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		
+		//for running failed testcases
+		//"rerun:target/failedRerun.txt"
+		},*/
+		
+		dryRun = false,
+		monochrome = true
+		)
+
+
+public class TestRunnerForDataDriven extends AbstractTestNGCucumberTests {
+
+
+}
